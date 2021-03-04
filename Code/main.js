8b936f7b78eb79d796797e9000a2bca6e05d84bd
Ballinger Guillaume <guillaume@ballinger.lu>
2021-03-04 09:52:10 +0100

    made an animation to move up

---
+++ Code/main.js
@@ -0,0 +1,39 @@
+function nextMoveLogic(FieldNumber,FieldsToPlay){
+  if(FieldsToPlay===1){
+    return;
+  }
+  else{
+    if(FieldNumber===10){
+      movingRightAnimation();
+    }
+    else{
+      movingUpAnimation(FieldNumber+1,FieldsToPlay-1);
+    }
+  }
+}
+
+function movingUpAnimation(FieldNumber,FieldsToPlay){
+ if(FieldsToPlay<=0){
+   return;
+ }
+ const delay = 0.5;
+ const distance = 100;
+ var tl = gsap.timeline({onComplete:nextMoveLogic,onCompleteParams: [FieldNumber,FieldsToPlay]});
+
+ tl.fromTo("#car",{y:-1*(FieldNumber)*distance},{duration:delay,y:-1*(FieldNumber)*distance + (distance/2),scaleX:2,scaleY:2,ease:"expoScale(1,2,power1.out)"})
+ .fromTo("#car",{y:-1*(FieldNumber)*distance + (distance/2),scaleX:2,scaleY:2},{duration:delay,y:-1*((FieldNumber+1))*distance,scaleX:1,scaleY:1,ease:"expoScale(2,1,power4.out)"},"<");
+ tl.play();
+}
+
+
+
+//TODO
+function movingRightAnimation(){
+
+}
+
+
+$(document).ready(function(){
+  movingUpAnimation(1,2);
+  //start();
+});
