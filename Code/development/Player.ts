e336eed8d48652e6bc6001be3d52f4a460b99ed4
Joje <nemesienjoachim@gmail.com>
2021-03-09 14:38:50 +0100

    Added Template Functions to Player.ts
    Now able to call the functions from Player.ts

--- Code/development/Player.ts
+++ Code/development/Player.ts
@@ -1,5 +1,61 @@
+import { Field } from "./Fields/Field";
+
 class Player {
-    constructor() {
+    isBot : boolean;
+    Money : number;
+    //Array[Property]
+    Bankrupt : boolean;
+
+    
+
+    constructor(isBot: boolean /*, pawn: Pawn, Array: Property*/) {
+        this.isBot = isBot;
+        this.Money = 1500;
+        this.Bankrupt = false;
+    }
+    
+    canBuy(cost:number):boolean{
+        if ((this.Money-cost) > 0){
+            return true;
+        }
+        return false;
+    }
+
+    buying(field : Field): void{
 
     }
+
+    exchange(field: Field, player : Player): void{
+
+    }
+
+    receive(field: Field): void{
+
+    }
+
+    move(): void{
+
+    }
+
+    goToErasmus(): void{
+
+    }
+
+    startBonus(): void{
+
+    }
+
+    isBankrupt(): boolean{
+        return this.Bankrupt;
+    }
+
+    init(): void{
+
+    }
+
+    forfeit(): void{
+        
+    }
+
 }
+
