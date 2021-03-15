ebb5d94c790654dbe45f016cb62579fa28572ae6
Ballinger Guillaume <guillaume@ballinger.lu>
2021-03-15 12:40:03 +0100

    Finished Prison  and event. Now auto choosing while closing.

--- Code/development/Fields/Field.ts
+++ Code/development/Fields/Field.ts
@@ -2,7 +2,7 @@
 import {Player} from "../Player";
 
 //INTERFACE HERE
-export  interface Field{
+export interface Field{
   name:string;
   Event(player:Player):void;
   CanBuy(player:Player):boolean;
