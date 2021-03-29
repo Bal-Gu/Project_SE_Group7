2931b2b8174d714d4facc0bc4118ce767ea505d2
Joneen <nemesienjoachim@gmail.com>
2021-03-29 22:37:25 +0200

    Accidentally deleted code in main.ts

--- Code/development/main.ts
+++ Code/development/main.ts
@@ -61,6 +61,33 @@ class main {
 
 }
 
+new main().launch();
+
+
+$("#mortageModal").click(function () {
+    $("#MorageModal").css("display", "block");
+    var output;
+    for (var i = 0; i < 4; i++){
+        output += "<tr>";
+        output += "<td style='background-color: #ADD8E6'>LLC</td>"
+        output += "<td>";
+        // random amount of stars
+        var stars = Math.floor(Math.random() * 6);
+        for (var j = 0; j < stars; j++) {
+            output += "&#9734;"
+        }
+        output += "</td>";
+        output += "<td><button><span style='height: 25px;width: 25px;background-color: green; border-radius: 50%; display: inline-block'></span></button</td>";
+        output += "<td><button><span style='height: 25px;width: 25px;background-color: red; border-radius: 50%; display: inline-block'></span></button></td>";
+        output += "<td>400</td>";
+        output += "</tr>";
+    }
+
+    output += "<tr><td colspan='5' style='text-align: right'>-400 <button>Accept</button></td></tr>"
+
+    $("#mortageTable").html(output);
+
+});
 
 
 
