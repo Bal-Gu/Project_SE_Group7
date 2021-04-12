7d2e6e986d7a161fbc385dc3e2fe182bd0e7128b
Ballinger Guillaume <guillaume@ballinger.lu>
2021-04-12 14:27:04 +0200

    Erasmus + double check + optional hidden + small improvement on trade

--- Code/development/main.ts
+++ Code/development/main.ts
@@ -1,7 +1,7 @@
 import {Player} from "./Player";
 import {Auction} from "./Events/Auction";
-import { PlayerSelection} from "./PlayerSelection";
-
+import {PlayerSelection} from "./PlayerSelection";
+import globals from "../globalVariable.json"
 import {Restplace} from "./Fields/Restplace";
 import {Mortage} from "./Events/mortage";
 import {Properties} from "./Fields/Properties";
@@ -28,7 +28,7 @@ declare var fallingCoins;
 export class main {
     PlayerArray: Player[] = [];
     StaticPlayerArray: Player[] = [];
-    dice:Dice = new Dice();
+    dice: Dice = new Dice();
     WinCondition: number;
     RoundNumber: number;
     ReferencePlayer: Player;
@@ -36,6 +36,7 @@ export class main {
     ConseqDoubles: number = 0;
     GameEnded: boolean = false;
     TurnEnded: boolean = false;
+    played: number = 0;
 
     async main() {
         this.buttonEvent();
@@ -44,6 +45,7 @@ export class main {
         this.InitializeFieldArray();
         await this.InitializePlayers();
         while (!this.GameEnded) {
+            this.updateButtons(this.ReferencePlayer)
             await this.EndOfATurn();
             this.NextTurn();
             this.PlayerArray.forEach(
@@ -65,16 +67,16 @@ export class main {
         }
     }
 
-    async EndTurnButton(){
+    async EndTurnButton() {
         let EndButton = $("#endTurnButton");
         let self = this;
-        EndButton.click(function(){
+        EndButton.click(function () {
             console.log("turn ended");
             self.TurnEnded = true;
         });
     }
 
-    ShowPlayerMoney(){
+    ShowPlayerMoney() {
         console.log("ShowPlayerMoney is called");
         this.StaticPlayerArray = this.PlayerArray.slice();
         this.PlayerArray.forEach(player => player.PlayerArray = this.StaticPlayerArray);
@@ -86,26 +88,27 @@ export class main {
     }
 
     //Will wait for a player to play its turn
-    async EndOfATurn(){
+    async EndOfATurn() {
+        this.played = 0;
         while (!this.TurnEnded) {
             await new Promise(r => setTimeout(r, 500));
             console.log("waiting");
         }
     }
 
-    InitializeQueue(): void{
-        for(let i = 0; i < this.PlayerArray.length; i++){
+    InitializeQueue(): void {
+        for (let i = 0; i < this.PlayerArray.length; i++) {
             this.dice.roll();
             this.PlayerArray[i].queue = this.dice.total();
         }
         let n = this.PlayerArray.length;
         //Bubble Sort, if the queue number between two players is the same, it won't change on purpose
-        for(let i = 0; i < n-1; i++){
-            for(let y = 0; y < n-i-1; y++){
-                if(this.PlayerArray[y].queue > this.PlayerArray[y+1].queue){
+        for (let i = 0; i < n - 1; i++) {
+            for (let y = 0; y < n - i - 1; y++) {
+                if (this.PlayerArray[y].queue > this.PlayerArray[y + 1].queue) {
                     let temp = this.PlayerArray[y];
-                    this.PlayerArray[y] = this.PlayerArray[y+1];
-                    this.PlayerArray[y+1] = temp;
+                    this.PlayerArray[y] = this.PlayerArray[y + 1];
+                    this.PlayerArray[y + 1] = temp;
                 }
             }
         }
@@ -114,10 +117,10 @@ export class main {
         $("#current-player").text(this.ReferencePlayer.name);
     }
 
-    NextTurn(): void{
+    NextTurn(): void {
         let temp = this.PlayerArray[0];
-        for(let i = 0; i < this.PlayerArray.length-1; i++){
-            this.PlayerArray[i] = this.PlayerArray[i+1];
+        for (let i = 0; i < this.PlayerArray.length - 1; i++) {
+            this.PlayerArray[i] = this.PlayerArray[i + 1];
         }
         this.PlayerArray[3] = temp;
         this.ReferencePlayer = this.PlayerArray[0];
@@ -129,10 +132,10 @@ export class main {
         this.WinCondition = (n == 1) ? 3000 : (n == 2) ? 4000 : 5000;
     }
 
-    async InitializePlayers(){
+    async InitializePlayers() {
         let ps: PlayerSelection = new PlayerSelection();
         ps.event();
-        while(!ps.StartTheGamePressed){
+        while (!ps.StartTheGamePressed) {
             await new Promise(r => setTimeout(r, 500));
         }
         ps.initializePlayers();
@@ -140,39 +143,34 @@ export class main {
         this.InitializeQueue();
     }
 
-    InitializeFieldArray(): void{
+    InitializeFieldArray(): void {
         this.FieldArray = [];
         let propertiesFile = require('../properties.json');
         let a, b, c, d;
         a = b = c = 0;
 
-        for(let i = 0; i < 40; i++){
-            if(i == 0 || i == 10){
-                let idle:Idle = new Idle();
+        for (let i = 0; i < 40; i++) {
+            if (i == 0 || i == 10) {
+                let idle: Idle = new Idle();
                 this.FieldArray.push(idle);
-            }
-            else if(i == 20){
-                let restplace:Restplace = new Restplace();
+            } else if (i == 20) {
+                let restplace: Restplace = new Restplace();
                 this.FieldArray.push(restplace);
-            }
-            else if(i == 30){
-                let gotoerasmus:GoToErasmus = new GoToErasmus();
+            } else if (i == 30) {
+                let gotoerasmus: GoToErasmus = new GoToErasmus();
                 this.FieldArray.push(gotoerasmus);
-            }
-            else if(i == 5 || i == 15 || i == 25 || i == 35){
+            } else if (i == 5 || i == 15 || i == 25 || i == 35) {
                 let station = propertiesFile.stations[a];
-                let b:Bus = new Bus(station.name);
+                let b: Bus = new Bus(station.name);
                 this.FieldArray.push(b);
                 a++;
-            }
-            else if(i == 12 || i == 28){
-                let parking =  propertiesFile.parkings[b];
-                let pa:Parking = new Parking(parking.name);
+            } else if (i == 12 || i == 28) {
+                let parking = propertiesFile.parkings[b];
+                let pa: Parking = new Parking(parking.name);
                 this.FieldArray.push(pa);
                 b++;
-            }
-            else if(i == 4 || i == 38) {
-                switch(i){
+            } else if (i == 4 || i == 38) {
+                switch (i) {
                     case 4:
                         let luxtax: Tax = new Tax("Luxury Tax");
                         this.FieldArray.push(luxtax);
@@ -182,16 +180,13 @@ export class main {
                         this.FieldArray.push(inctax);
                         break;
                 }
-            }
-            else if(i == 2 || i == 17 || i == 33){
-                let eventfield:EventField = new EventField();
+            } else if (i == 2 || i == 17 || i == 33) {
+                let eventfield: EventField = new EventField();
                 this.FieldArray.push(eventfield);
-            }
-            else if(i == 7 || i == 22 || i == 36){
-                let quizfield:QuizField = new QuizField();
+            } else if (i == 7 || i == 22 || i == 36) {
+                let quizfield: QuizField = new QuizField();
                 this.FieldArray.push(quizfield);
-            }
-            else {
+            } else {
                 let prop = propertiesFile.properties[c];
                 let p: Properties = new Properties(prop.color, prop.pricetopay, prop.renovationscosts, prop.name, prop.initialprice);
                 this.FieldArray.push(p);
@@ -201,39 +196,39 @@ export class main {
 
     }
 
-    SaveGameState(n: number, p: Player):void{
+    SaveGameState(n: number, p: Player): void {
 
     }
 
-    async MakePlayerTurn(): Promise<void>{
+    async MakePlayerTurn(): Promise<void> {
         let erasmus = new Erasmus()
         let double = this.dice.isdouble();
 
-        if (this.ReferencePlayer.TurnsInPrison > 0){
+        if (this.ReferencePlayer.TurnsInPrison > 0) {
             await erasmus.Event(this.ReferencePlayer, this.StaticPlayerArray);
         }
-        if(double){
-            if(this.ConseqDoubles >= 2){
+        if (double) {
+            if (this.ConseqDoubles >= 2) {
                 this.ReferencePlayer.TurnsInPrison = 3;
                 this.ConseqDoubles = 0;
-            }
-            else{
+            } else {
                 this.ConseqDoubles += 1;
             }
-        }else{
+        } else {
             this.ConseqDoubles = 0;
         }
         this.StaticPlayerArray.forEach(playerobject => console.log(playerobject.name));
         this.FieldArray[this.ReferencePlayer.currentposition].Event(this.ReferencePlayer, this.StaticPlayerArray);
     }
 
-    CheckWinCondition(player:Player):void{
-        if (player.Money >= this.WinCondition){
+    CheckWinCondition(player: Player): void {
+        if (player.Money >= this.WinCondition) {
             this.GameEnded = true;
         }
     }
-    CheckLooseCondition(player:Player):void{
-        if (player.isGameOver){
+
+    CheckLooseCondition(player: Player): void {
+        if (player.isGameOver) {
             this.Surrender(player);
         }
     }
@@ -255,15 +250,15 @@ export class main {
         console.log("c");
     }
 
-    async SetMortgageTest(){
+    async SetMortgageTest() {
         let p: Player = new Player(false, "f", 0);
         this.playerInit(p);
         let repay = new setMortgage();
-       await repay.event(p);
+        await repay.event(p);
     }
 
 
-    async RepayMortgageTest(){
+    async RepayMortgageTest() {
         let p: Player = new Player(false, "f", 0);
         this.playerInit(p);
         let repay = new RepayMortgage();
@@ -284,71 +279,71 @@ export class main {
     async MortageTest() {
         let p: Player = new Player(false, "f", 0);
         this.playerInit(p);
-        for(let i=0;i<30;i++){
+        for (let i = 0; i < 30; i++) {
             var rand = Math.floor(Math.random() * Object.keys(Colors).length);
-            var randColorValue:Colors = Colors[Object.keys(Colors)[rand]];
-        let prop2:Properties = new Properties(randColorValue, [1,2,3,4],20,"Not today",200);
-        prop2.renovatiosAmmount = 3;
-        prop2.isMortgage = false;
-        p.fieldsOwned.push(prop2);
+            var randColorValue: Colors = Colors[Object.keys(Colors)[rand]];
+            let prop2: Properties = new Properties(randColorValue, [1, 2, 3, 4], 20, "Not today", 200);
+            prop2.renovatiosAmmount = 3;
+            prop2.isMortgage = false;
+            p.fieldsOwned.push(prop2);
         }
 
         await new Mortage().event(p);
 
-        for(let i=0;i<p.fieldsOwned.length;i++){
+        for (let i = 0; i < p.fieldsOwned.length; i++) {
             console.log(p.fieldsOwned[i].renovatiosAmmount);
         }
 
     }
 
-    async TradeTest(){
-        let p1:Player = new Player(false,"ad", 0);
-        let p2:Player = new Player(false,"bc", 1);
+    async TradeTest() {
+        let p1: Player = new Player(false, "ad", 0);
+        let p2: Player = new Player(false, "bc", 1);
         this.playerInit(p1);
         this.playerInit(p2);
         p1.hasErasmusDispense = true;
-        await new Trade().event(p1,p2)
+        await new Trade().event(p1, p2)
         console.log("P1");
-        console.log("P1 has erasmus =>"+p1.hasErasmusDispense);
+        console.log("P1 has erasmus =>" + p1.hasErasmusDispense);
         p1.fieldsOwned.forEach((value) => {
-           console.log(value.name);
+            console.log(value.name);
         });
         console.log("P2");
-        console.log("P2 has erasmus =>"+p2.hasErasmusDispense);
+        console.log("P2 has erasmus =>" + p2.hasErasmusDispense);
         p2.fieldsOwned.forEach((value) => {
-           console.log(value.name);
+            console.log(value.name);
         });
 
     }
 
-    async BuyTest(){
+    async BuyTest() {
         let p: Player = new Player(false, "f", 0);
-        let p2: Player = new Player(false,"yieks", 1)
+        let p2: Player = new Player(false, "yieks", 1)
         p.Money = 1000;
-        let prop:Properties = new Properties(Colors.Light_Blue, [1,2,3,4],10,"lel",100);
+        let prop: Properties = new Properties(Colors.Light_Blue, [1, 2, 3, 4], 10, "lel", 100);
         prop.renovatiosAmmount = 3;
         prop.isMortgage = false;
         let buyevent = new BuyEvent();
-        await buyevent.event(p,700,prop,[p,p2]);
+        await buyevent.event(p, 700, prop, [p, p2]);
     }
 
-    playerInit(p:Player){
+    playerInit(p: Player) {
 
         p.Money = 1000;
-        let prop:Properties = new Properties(Colors.Light_Blue, [1,2,3,4],10,"lel",100);
+        let prop: Properties = new Properties(Colors.Light_Blue, [1, 2, 3, 4], 10, "lel", 100);
         prop.renovatiosAmmount = 0;
         prop.isMortgage = false;
         p.fieldsOwned.push(prop);
-        let prop2:Properties = new Properties(Colors.Light_Blue, [1,2,3,4],10,"lel",10000);
+        let prop2: Properties = new Properties(Colors.Light_Blue, [1, 2, 3, 4], 10, "lel", 10000);
         prop2.renovatiosAmmount = 0;
         prop2.isMortgage = true;
         p.fieldsOwned.push(prop2);
     }
 
-    async buttonEvent(){
+    async buttonEvent() {
         let self = this;
         // show the start game modal
-        $("#startGame").click(function() {
+        $("#startGame").click(function () {
             $("#startGameModal").show();
         });
         // close any modal that has a close X
@@ -370,16 +365,22 @@ export class main {
             fallingCoins('body');
         });
         // to roll the dices
-        $("#rollButton").click(function(){
+        $("#rollButton").click(function () {
             //handle the doubles
             self.dice.roll();
             self.ReferencePlayer.move(self.dice.total());
-            setTimeout(function(){
+            setTimeout(function () {
                 self.MakePlayerTurn();
-            },self.dice.total()*500);
+            }, self.dice.total() * 500);
+            this.hidden = !self.dice.isdouble();
+            self.played += 1;
+            if(self.played == 3 && self.dice.isdouble()){
+                self.ReferencePlayer.goToErasmus();
+            }
+
         });
         // to test the quiz modal
-        $("#quizButton").click(async ()=>{
+        $("#quizButton").click(async () => {
             await new Quiz().event();
         });
         $("#sellMortageProButton").click(async function(){
@@ -389,12 +390,75 @@ export class main {
             new RepayMortgage().event(self.ReferencePlayer);
         });
     }
-    updateButtons(){
 
+    updateButtons(p: Player) {
+        //SELL RENOVATIONS
+        let renovationSell = $("#sellRenovationsButton");
+        let cansell: boolean = false;
+        for (let i = 0; p.fieldsOwned.length; i++) {
+            // @ts-ignore
+            if (p.fieldsOwned[i].isMortgage != undefined && !p.fieldsOwned[i].isMortgage && p.fieldsOwned[i].renovatiosAmmount != undefined && p.fieldsOwned[i].renovatiosAmmount > 0) {
+                cansell = true;
+            }
+        }
+        if (cansell) {
+            renovationSell.show();
+        } else {
+            renovationSell.hide();
+        }
+        //Set Mortgage properties
+        let SellMortage = $("#sellMortageProButton");
+        cansell = false;
+        for (let i = 0; p.fieldsOwned.length; i++) {
+            // @ts-ignore
+            if (p.fieldsOwned[i].isMortgage != undefined && !p.fieldsOwned[i].isMortgage && p.fieldsOwned[i].renovatiosAmmount != undefined && p.fieldsOwned[i].renovatiosAmmount == 0) {
+                cansell = true;
+            }
+        }
+        if (cansell) {
+            SellMortage.show();
+        } else {
+            SellMortage.hide();
+        }
+        //Buy renovations
+        let Buyrenovation = $("#buyRenovationButton");
+        cansell = false;
+        for (let i = 0; p.fieldsOwned.length; i++) {
+            // @ts-ignore
+            if (p.fieldsOwned[i].isMortgage != undefined && !p.fieldsOwned[i].isMortgage && p.fieldsOwned[i].renovatiosAmmount != undefined && p.fieldsOwned[i].renovatiosAmmount < globals.MaxRenovations) {
+                cansell = true;
+            }
+        }
+        if (cansell) {
+            Buyrenovation.show();
+        } else {
+            Buyrenovation.hide();
+        }
+        //End turn → skip that one because always on
+        $("#endTurnButton").show();
+        //Menu Button → skip that one because always on
+        $("#startMenuButton").show();
+        //Trade
+        $("#tradeButton").show()
+        //Repay mortgage
+        let repaymortgage = $("#repayMortgageButton");
+        let minimumPrice: number = 20000000000;
+        for (let i = 0; p.fieldsOwned.length; i++) {
+            if (p.fieldsOwned[i].isMortgage) {
+                minimumPrice = minimumPrice < p.fieldsOwned[i].initialPrice ? minimumPrice : p.fieldsOwned[i].initialPrice;
+            }
+        }
+        if (p.canBuy(minimumPrice)) {
+            repaymortgage.show();
+        } else {
+            repaymortgage.hide();
+        }
+        //Roll dice
+        $("#rollButton").show();
     }
 }
 
-$("#quizButton").click(()=>{
+$("#quizButton").click(() => {
     $("#QuestionModal").show();
 });
 
