c2cad1a932275fb512685d332510cd7a448e1bda
David Matos Freitas <davidfreitas-98@hotmail.com>
2021-04-26 22:10:37 +0200

    Modal fun facts of the buildings added

--- Code/development/main.ts
+++ Code/development/main.ts
@@ -721,6 +721,19 @@ export class main {
                 counter = 1;
             }
         });
+
+        $(".buildingFunFact").click(function () {
+            // @ts-ignore
+            const buildingTitle = this.lastElementChild.firstChild.nextSibling.innerHTML;
+            // @ts-ignore
+            const buildingCredits = this.lastElementChild.lastElementChild.innerHTML;
+            // @ts-ignore
+            const imageName = this.lastElementChild.id;
+            $("#buildingFunFactTitle").html(buildingTitle + " ("+buildingCredits+")");
+            $("#imageBuilding").html("<img src='./graphic/images/large/"+imageName+".jpg' style='width: 100%;'>");
+            // TODO: the building discription needs to me added 
+            $("#funfactModal").show();
+        });
     }
 
     updateButtons(p: Player) {
