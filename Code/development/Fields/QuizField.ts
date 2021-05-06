0306a6cf348a0dab71a7231f637fec990f121800
Ballinger Guillaume <guillaume@ballinger.lu>
2021-05-07 01:36:21 +0200

    creating new quizfield which will make the event none overlapping

--- Code/development/Fields/QuizField.ts
+++ Code/development/Fields/QuizField.ts
@@ -7,14 +7,14 @@ export class QuizField implements Field{
     initialPrice: number;
     name: string;
     owner: Player | undefined;
-    quiz:Quiz = new Quiz();
 
     CanBuy(player: Player): boolean {
         return false;
     }
 
-    Event(player: Player, playerList: Player[]): void {
-        this.quiz.event(player);
+    async Event(player: Player, playerList: Player[]): Promise<void> {
+        let quiz:Quiz = new Quiz();
+        await quiz.event(player);
     }
 
 }
