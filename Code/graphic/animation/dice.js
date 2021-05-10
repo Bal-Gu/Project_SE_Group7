3eb76f94f35fda0282d8ab0679f286e76fdff3f9
vallere-samuel <sametjosse57@gmail.com>
2021-05-10 11:06:39 +0200

    dices bug fixed

--- Code/graphic/animation/dice.js
+++ Code/graphic/animation/dice.js
@@ -32,11 +32,6 @@ function rolldices(numberDice1, numberDice2, playerNr, playerName) {
     $("#dices"+(playerNr+1)).hide();
     rollButton.prop("disabled", false);
   }, 3000);
-
-/*$("#dices"+(0+1)).show();
-$("#dices"+(1+1)).show();
-$("#dices"+(2+1)).show();
-$("#dices"+(3+1)).show(); */
 }
 
 function getRandom(max) {
